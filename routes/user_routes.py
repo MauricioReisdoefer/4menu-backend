3890a0169fe6131abe8dddd17c84b5132c8e7afa
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-08-31 13:43:08 -0300

    FEAT: Creating user routes

---
+++ routes/user_routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint
+from flask_jwt_extended import jwt_required
+from controllers.user_controller import (
+    create_user,
+    login,
+    get_users,
+    get_user,
+    update_user,
+    delete_user
+)
+
+user_bp = Blueprint("users", __name__, url_prefix="/users")
+
+user_bp.route("/create", methods=["POST"])(create_user)
+user_bp.route("/login", methods=["POST"])(login)
+user_bp.route("/list", methods=["GET"])(get_users)
+user_bp.route("/<int:user_id>", methods=["GET"])(get_user)
+user_bp.route("/update/<int:user_id>", methods=["PUT"])(jwt_required()(update_user))
+user_bp.route("/delete/<int:user_id>", methods=["DELETE"])(jwt_required()(delete_user))
--- run.py
+++ run.py
@@ -1,23 +1,23 @@
 from flask import Flask
 from flask_cors import CORS
 from db import db
-from controllers.user_controllers import user_bp
+from routes.user_routes import user_bp
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     
-    app.config["JWT_SECRET_KEY"] = "fodase"
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     jwt = JWTManager(app)
         
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydb.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
     db.init_app(app)
     CORS(app)  
     app.register_blueprint(user_bp)
     return app
-
 
 if __name__ == "__main__":
     app = create_app()
