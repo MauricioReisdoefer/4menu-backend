8a184e89273bb29ba39be314180147e309200582
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-08-31 20:05:11 -0300

    FEAT: Creating item model

---
+++ models/item_model.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+from jsonlite import JsonTable, JsonQuerier
+
+# ---------------------------
+# Modelo da Seção
+# ---------------------------
+@dataclass
+class Item:
+    _id: Optional[int] = field(default=None)
+    name: str = ""
+    description: str = ""
+    price: float = 0.00
+    section_id: int = 0
+    created_at: str = field(default_factory=lambda: datetime.utcnow().isoformat())
+    updated_at: str = field(default_factory=lambda: datetime.utcnow().isoformat())
+
+# ---------------------------
+# Tabela JSON
+# ---------------------------
+item_table = JsonTable("tables/item.json", Item)
+item_querier = JsonQuerier(item_table)
